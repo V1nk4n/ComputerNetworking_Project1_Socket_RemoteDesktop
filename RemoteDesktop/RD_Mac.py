04bdf6c16d73831b83a02f225e415eb8b45ad7c3
Your Name <you@example.com>
2024-01-10 14:08:00 +0700

    hi

--- RemoteDesktop/RD_Mac.py
+++ RemoteDesktop/RD_Mac.py
@@ -1,6 +1,8 @@
-import uuid
 from RD_Constant import FORMAT
+from uuid import getnode as get_mac
+
+mac = get_mac()
 
 def mac_addr(com_con):
-    com_con.sendall(hex(uuid.getnode()).encode(FORMAT))
+    com_con.sendall(hex(mac).encode(FORMAT))
     return
